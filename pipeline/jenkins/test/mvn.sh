cc40c5e1e939b63c550145271469c747d87c5259
sagar.mooli@cloudzenix.com <msvv.sagarreddy2501@gmail.com>
2023-03-31 06:35:25 +0000

    workspace fixes

--- pipeline/jenkins/test/mvn.sh
+++ pipeline/jenkins/test/mvn.sh
@@ -3,6 +3,7 @@
 echo "***************************"
 echo "** Testing the code ***********"
 echo "***************************"
-WORKSPACE=/home/ec2-user/jenkins/jenkins_home/workspace/pipeline-docker-maven
+
+WORKSPACE=WORKSPACE=/home/ec2-user/jenkins/jenkins_home/workspace/Demo-pipeline
 
 docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven "$@"

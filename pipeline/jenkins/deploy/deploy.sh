e7e60d27edf2d611fe416f3f647be389b8030d88
sagar.mooli@cloudzenix.com <msvv.sagarreddy2501@gmail.com>
2023-03-31 07:03:07 +0000

    minor changes to tf and deploy.sh

--- pipeline/jenkins/deploy/deploy.sh
+++ pipeline/jenkins/deploy/deploy.sh
@@ -4,6 +4,6 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /opt/prod /tmp/.auth prod-user@linuxfacilito.online:/tmp/.auth
-scp -i /opt/prod ./jenkins/deploy/publish prod-user@linuxfacilito.online:/tmp/publish
-ssh -i /opt/prod prod-user@linuxfacilito.online "/tmp/publish"
+scp -i /opt/prod /tmp/.auth dev-user@ip-172-31-84-120:/tmp/.auth
+scp -i /opt/prod ./jenkins/deploy/publish dev-user@ip-172-31-84-120:/tmp/publish
+ssh -i /opt/prod dev-user@ip-172-31-84-120 "/tmp/publish"

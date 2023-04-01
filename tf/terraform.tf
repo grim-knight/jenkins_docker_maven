c8768c2ecdae5a363d1fb992137bde8c79740c8a
sagar.mooli@cloudzenix.com <msvv.sagarreddy2501@gmail.com>
2023-04-01 22:35:48 +0000

    terraform script updated

--- tf/terraform.tf
+++ tf/terraform.tf
@@ -12,12 +12,19 @@ resource "aws_security_group" "flask_app_sg" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress{
+	from_port = 30008
+	to_port = 30008
+	protocol = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]
+}
  
   ingress {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = ["70.120.100.125/32","52.54.254.122/32","24.242.173.10/32"]
+    cidr_blocks = ["70.120.100.125/32","52.54.254.122/32","44.195.129.92/32","24.242.173.10/32","54.161.114.126/32"]
  }
 
  egress{
@@ -31,18 +38,26 @@ resource "aws_security_group" "flask_app_sg" {
 # Create an EC2 instance for the Flask application
 resource "aws_instance" "flask_app_instance" {
   ami           = "ami-0aa7d40eeae50c9a9"
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
   key_name = "sagartest"
   vpc_security_group_ids = [aws_security_group.flask_app_sg.id]
 
   # Define the user data script to install the necessary software and start the Flask application
   user_data = <<-EOF
               #!/bin/bash
-              sudo apt-get update
-		sudo yum install python37
-               curl -O https://bootstrap.pypa.io/get-pip.py
-		python3 get-pip.py --user
-              sudo pip3 install flask
+                sudo yum update -y
+              sudo yum install -y docker
+              sudo service docker start
+              sudo usermod -a -G docker ec2-user
+              sudo yum install -y python3-pip
+              sudo pip3 install docker-compose
+              sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+              sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+              sudo curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+              sudo install minikube-linux-amd64 /usr/local/bin/minikube
+              sudo curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubeadm
+              sudo install -o root -g root -m 0755 kubeadm /usr/local/bin/kubeadm
+	        sudo pip3 install flask
 		echo "from flask import Flask; app = Flask(__name__);" > /home/ec2-user/app.py
 		echo "if __name__ == '__main__': app.run(host='0.0.0.0')" >> /home/ec2-user/app.py
               sudo nohup python3 /home/ubuntu/app.py > /dev/null 2>&1 &

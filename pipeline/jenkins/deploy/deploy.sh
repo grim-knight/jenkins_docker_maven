162bcec9f1dd6dee1b7654e80b2597c53f6ac50c
root <root@ip-10-1-29-165.ec2.internal>
2023-04-07 03:42:05 +0000

    trivial shit

--- pipeline/jenkins/deploy/deploy.sh
+++ pipeline/jenkins/deploy/deploy.sh
@@ -4,6 +4,6 @@ echo docker-jenkins-maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-rsync -avz -e "ssh -i /opt/prod" /tmp/.auth ec2-user@3.93.177.29:/tmp/
-rsync -avz -e "ssh -i /opt/prod" /home/ec2-user/jenkins/jenkins_docker_maven/pipeline/jenkins/deploy/publish ec2-user@3.93.177.29:/tmp/publish
-rsync -avz -e "ssh -i /opt/prod" ec2-user@3.93.177.29 "/tmp/publish"
+rsync -avz -e "ssh -i /home/ec2-user/jenkins/jenkins_docker_maven/tf/prod" /tmp/.auth ec2-user@3.93.177.29:/tmp/
+rsync -avz -e "ssh -i /home/ec2-user/jenkins/jenkins_docker_maven/tf/prod" /home/ec2-user/jenkins/jenkins_docker_maven/pipeline/jenkins/deploy/publish ec2-user@3.93.177.29:/tmp/publish
+rsync -avz -e "ssh -i /home/ec2-user/jenkins/jenkins_docker_maven/tf/prod" ec2-user@3.93.177.29 "/tmp/publish"

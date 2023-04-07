59a8c9660600b573127aa872de781b9c3d867271
root <root@ip-10-1-29-165.ec2.internal>
2023-04-07 04:12:18 +0000

    debuggingv.0.2

--- pipeline/jenkins/deploy/deploy.sh
+++ pipeline/jenkins/deploy/deploy.sh
@@ -8,4 +8,5 @@ rsync -avz -e "ssh -i /opt/prod" /tmp/.auth ec2-user@3.93.177.29:/tmp/
 echo "i got executed"
 rsync -avz -e "ssh -i /opt/prod" /home/ec2-user/jenkins/jenkins_docker_maven/pipeline/jenkins/deploy/publish ec2-user@3.93.177.29:/tmp/publish
 echo "i got executed"
-rsync -avz -e "ssh -i /opt/prod" ec2-user@3.93.177.29 "/tmp/publish"
+ssh -i /opt/prod ec2-user@3.93.177.29 "/tmp/publish"
+echo "Finally success"
